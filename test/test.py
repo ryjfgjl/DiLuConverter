4af5b7d77e455ca78c0fe64e72e75b9646f24a76
ryjfgjl <2577154121@qq.com>
2022-09-06 13:30:35 +0800

    Release ExcelToDatabase 5.1

--- test/test.py
+++ test/test.py
@@ -1,27 +1,59 @@
 import PySimpleGUI as sg
-import time
+import hashlib
 
-def my_function():
-    time.sleep(30)
+'''
+     Create a secure login for your scripts without having to include your password in the program. Create an SHA1 hash code for your password using the GUI. Paste into variable in final program
+     1. Choose a password
+     2. Generate a hash code for your chosen password by running program and entering 'gui' as the password
+     3. Type password into the GUI
+     4. Copy and paste hash code Window GUI into variable named login_password_hash
+     5. Run program again and test your login!
+'''
 
-def my_function_with_parms(duration):
-    time.sleep(duration)
-    return 'My Return Value'
+# Use this GUI to get your password's hash code
+def HashGeneratorGUI():
+    layout = [[sg.T('Password Hash Generator', size=(30,1), font='Any 15')],
+              [sg.T('Password'), sg.In(key='password')],
+              [sg.T('SHA Hash'), sg.In('', size=(40,1), key='hash')],
+              ]
 
-layout = [  [sg.Text('Call a lengthy function')],
-            [sg.Button('Start'), sg.Button('Start 2'), sg.Button('Exit')]  ]
+    window = sg.Window('SHA Generator', layout, auto_size_text=False, default_element_size=(10,1),
+                       text_justification='r', return_keyboard_events=True, grab_anywhere=False)
 
-window = sg.Window('Long Operation Example', layout)
 
-while True:
-    event, values = window.read()
-    print(event, values)
-    if event == sg.WIN_CLOSED or event == 'Exit':
-        break
-    if event == 'Start':
-        window.perform_long_operation(my_function, '-FUNCTION COMPLETED-')
-    elif event == 'Start 2':
-        window.perform_long_operation(lambda: my_function_with_parms(10), '-FUNCTION COMPLETED-')
-    elif event == '-FUNCTION COMPLETED-':
-        sg.popup('Your function completed!')
-window.close()
+    while True:
+        event, values = window.read()
+        if event ==  sg.WIN_CLOSED:
+              exit(69)
+
+        password = values['password']
+        try:
+            password_utf = password.encode('utf-8')
+            sha1hash = hashlib.sha1()
+            sha1hash.update(password_utf)
+            password_hash = sha1hash.hexdigest()
+            window['hash'].update(password_hash)
+        except:
+            pass
+
+# ----------------------------- Paste this code into your program / script -----------------------------
+# determine if a password matches the secret password by comparing SHA1 hash codes
+def PasswordMatches(password, hash):
+    password_utf = password.encode('utf-8')
+    sha1hash = hashlib.sha1()
+    sha1hash.update(password_utf)
+    password_hash = sha1hash.hexdigest()
+    if password_hash == hash:
+        return True
+    else:
+        return False
+
+login_password_hash = '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8'
+password = sg.popup_get_text('1', password_char='*')
+if password == 'gui':                # Remove when pasting into your program
+    HashGeneratorGUI()               # Remove when pasting into your program
+    exit(69)                         # Remove when pasting into your program
+if PasswordMatches(password, login_password_hash):
+    print('Login SUCCESSFUL')
+else:
+    print('Login FAILED!!')
